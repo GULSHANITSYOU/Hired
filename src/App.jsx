2bfdcff3a403014d26987674e0865fdd76a9ad78
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-10-06 22:20:28 +0530

    Shad cn setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layout/app-layout";
+import LandingPage from "./pages/landing";
+
+const router = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children: [{
+      path: "/",
+      element: <LandingPage/>
+    }],
+  }
+])
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
