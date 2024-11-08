a498aa91cba5b2656b0982f83ef2404e5d3665eb
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-11-08 09:15:04 +0530

    Project Abode

--- src/pages/Onboarding.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Onboarding = () => {
-  return <div>onboarding</div>;
-};
-
-export default Onboarding;
--- src/pages/landingpage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LandinPage = () => {
-  return <div className=""> landing page </div>
-};
- 
-export default LandinPage;
